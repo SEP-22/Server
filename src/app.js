1241c8cfb24bce6ba96c789b6240b9e7d9f543b7
GimhanRanasinghe <gimhanravi@gmail.com>
2022-09-27 16:17:12 +0530

    signup connected with backend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 const bodyParser = require("body-parser");
 const router = require("./router/_index.routes");
 const app = express();
+
+let origins = "*";
+
+if (process.env.ALLOWED_ORIGINS) {
+  origins = process.env.ALLOWED_ORIGINS.split(",");
+}
+
+app.use(
+  cors({
+    origin: origins,
+    exposedHeaders: ["x-refresh-token", "x-access-token"],
+  })
+);
+
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
