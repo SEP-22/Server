7355757f8b2fb8b898f6f59ad1a5a0642ac89dee
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-09-28 09:42:07 +0530

    food by category

--- src/controller/food.controller.js
+++ src/controller/food.controller.js
@@ -1,67 +1,113 @@
-const Food = require('../models/food');
-const mongoose = require('mongoose')
+const Food = require("../models/food");
 
 // get all food
 const getFoods = async (req, res) => {
-    const foods = await Food.find({})
+  const foods = await Food.find({});
 
-    res.status(200).json(foods)
-}
+  res.status(200).json(foods);
+};
 
+//get all food by category
+const foodsByCategory = async (req, res) => {
+  const Vegetables_Fruits = await Food.find({
+    category: "Fruits and Vegetables",
+  });
+  const StarchyFood = await Food.find({ category: "Starchy food" });
+  const Proteins = await Food.find({ category: "Proteins" });
+  const Dairy_Fat = await Food.find({ category: "Dairy and Fats" });
+  const Sugar = await Food.find({ category: "Sugar" });
+
+  data = {
+    "Fruits and Vegetables": Vegetables_Fruits,
+    "Starchy food": StarchyFood,
+    "Proteins": Proteins,
+    "Dairy and Fats": Dairy_Fat,
+    "Sugar": Sugar,
+  };
+
+  res.status(200).json(data);
+};
 
 // add new food
 const addfood = async (req, res) => {
-    const {name, cal_per_gram, category, diabetics, cholesterol, bloodpressure, protein, fat, fiber, carbs, image } = req.body
+  const {
+    name,
+    cal_per_gram,
+    category,
+    diabetics,
+    cholesterol,
+    bloodpressure,
+    protein,
+    fat,
+    fiber,
+    carbs,
+    image,
+  } = req.body;
 
-    try {
-        const food = await Food.create({name, cal_per_gram, category, diabetics, cholesterol, bloodpressure, protein, fat, fiber, carbs, image })
-        res.status(200).json(food)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-}
-
+  try {
+    const food = await Food.create({
+      name,
+      cal_per_gram,
+      category,
+      diabetics,
+      cholesterol,
+      bloodpressure,
+      protein,
+      fat,
+      fiber,
+      carbs,
+      image,
+    });
+    console.log(food);
+    res.status(200).json(food);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // edit food
 const editFood = async (req, res) => {
-    const { id } = req.params
-  
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({error: 'No such food'})
-    }
-  
-    const food = await Food.findOneAndUpdate({_id: id}, {
-      ...req.body
-    })
-  
-    if (!food) {
-      return res.status(400).json({error: 'No such food'})
-    }
-  
-    res.status(200).json(food)
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such food" });
   }
 
+  const food = await Food.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!food) {
+    return res.status(400).json({ error: "No such food" });
+  }
+
+  res.status(200).json(food);
+};
 
 // delete food
 const deleteFood = async (req, res) => {
-    const { id } = req.params
-  
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({error: 'No such food'})
-    }
-  
-    const food = await Food.findOneAndDelete({_id: id})
-  
-    if(!food) {
-      return res.status(400).json({error: 'No such food'})
-    }
-  
-    res.status(200).json(food)
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such food" });
   }
 
+  const food = await Food.findOneAndDelete({ _id: id });
+
+  if (!food) {
+    return res.status(400).json({ error: "No such food" });
+  }
+
+  res.status(200).json(food);
+};
+
 module.exports = {
-    getFoods,
-    addfood,
-    editFood,
-    deleteFood,
-  };
+  getFoods,
+  addfood,
+  editFood,
+  deleteFood,
+  foodsByCategory,
+};
--- src/router/food.routes.js
+++ src/router/food.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
-const { getFoods, addfood, editFood, deleteFood, } = require("../controller/food.controller");
+const { getFoods, addfood, editFood, deleteFood, foodsByCategory, } = require("../controller/food.controller");
 
 const router = Router();
 
 router.post("/newFood", addfood);
 router.patch("/editFood/:id", editFood);
 router.delete("/deleteFood/:id", deleteFood);
-router.get("/", getFoods);
+router.get("/allfoods", getFoods);
+router.get("/foodbycategory", foodsByCategory);
 
 
 module.exports.foodRouter = router;
