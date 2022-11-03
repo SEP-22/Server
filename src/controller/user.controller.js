7bb7b45352e21da06012ca6ac0887d0851fb835f
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-03 17:57:53 +0530

    some changes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -148,6 +148,13 @@ const updateActiveDietPlan = async (req, res) => {
   }
   res.status(200).json(user);
 };
+//update userprofile details
+const editProfile = async (req, res) => {
+  const { id } = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: "No such user"})
+  }
+}
 
 //////////////////////// ING ////////////////////////
 
