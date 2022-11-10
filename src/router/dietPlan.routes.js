769a5661dd0dd96b13739fa29f4ddf9be79391d4
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-10 16:56:06 +0530

    dietplan controller update

--- src/router/dietPlan.routes.js
+++ src/router/dietPlan.routes.js
@@ -3,6 +3,7 @@ const {
   getInputs,
   getDietPlanById,
   generateDietPlan,
+  getDietPlanByUserId,
 } = require("../controller/dietPlan.controller");
 const {saveDietPlans, getDietPlans} = require("../controller/diet.controller")
 const auth = require("../middleware/auth");
@@ -14,5 +15,6 @@ router.get("/getplans/getDietPlans",getDietPlans);
 router.get("/:id", getDietPlanById);
 router.post("/generatedietplan", generateDietPlan);
 router.post("/savedietplan", saveDietPlans);
+router.get("/getUserDietPlans/:id",getDietPlanByUserId);
 
 module.exports.dietPlanRouter = router;
