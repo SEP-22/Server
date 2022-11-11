5bcf732e3c30e0e860f696808814aa8f9ab4993f
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-11 11:32:09 +0530

    generate shoppinglist

--- src/controller/diet.controller.js
+++ src/controller/diet.controller.js
@@ -59,8 +59,146 @@ const getDietPlans = async (req, res) => {
   //sending only first 3 -- change this later
   const reply = dietPlan.slice(0,3);
   res.status(200).json(reply)
-}  
+};
+const saveShoppingList = async(req,res) => {
+  const  plans  = req.body;
+  // const plans  = [{dietPlan_Id:1,breakfast:[["bf",1,2],["bf1",1,2]],lunch:[["ln",1,2],["ln1",1,2]],dinner:[["dn",1,2],["dn1",1,2]]},
+  //                 {dietPlan_Id:2,breakfast:[["bf2",1,2],["bf21",1,2]],lunch:[["ln2",1,2],["ln21",1,2]],dinner:[["dn2",1,2],["dn21",1,2]]}];
+
+  arr = [];
+  plans.forEach((e) => {
+    arr.push({
+      dietPlan_Id: e.dietPlan_Id,
+      breakfast: e.breakfast,
+      lunch: e.lunch,
+      dinner: e.dinner,
+    });
+  });
+  shoppingListArr = [];
+  var compDet = {};
+
+  const quotient = Math.floor(7/arr.length);
+  const remainder = 7 % arr.length;
+
+  console.log(quotient,remainder)
+  //creating newarray with 7 dietplans
+  let newArr = [];
+  for (let i = 0; i < quotient; i++) {
+    newArr = newArr.concat(arr);
+  }
+  newArr = newArr.concat(arr.slice(0,remainder))
+  console.log(newArr.length)
+
+  newArr.forEach((e) => {
+    breakfastArr = e.breakfast;
+    lunchArr = e.lunch;
+    dinnerArr = e.dinner;
+    //breakfast loop
+    breakfastArr.forEach((br) => {
+      //shoppingListArr.push(br[2]);
+
+      //corr start
+      // let len = br[2].length
+      // if(compDet[br[4]]){
+      //   let temp = compDet[br[4]];
+      //   compDet[br[4]] = temp*1 + (br[2].substring(0, len-2))*1;
+      //   //document.getElementById("demo2").innerHTML = compDet[br[4]];
+      // }else{
+      //   compDet[br[4]]=(br[2].substring(0, len-2))*1;
+      // }
+      //corr end
+      let amountLen = br[2].length
+      let calorieLen = br[1].length
+      let amount = br[2].substring(0,amountLen-2)
+      let calorie = br[1].substring(0,calorieLen-4)
+
+      if(compDet[br[4]]){
+        //console.log("magulaaa",compDet[br[4]])
+        let oldAmount = compDet[br[4]][0];
+        let oldCalorie = compDet[br[4]][1];
+        let newAmount = oldAmount + amount*1;
+        let newCalorie = oldCalorie + calorie*1;
+        compDet[br[4]] = [newAmount,newCalorie];
+      }else{
+        compDet[br[4]] = [amount*1,calorie*1];
+      }
+    });
+    lunchArr.forEach((br) => {
+      //shoppingListArr.push(br[2]);
+
+      //corr start
+      // let len = br[2].length
+      // if(compDet[br[4]]){
+      //   let temp = compDet[br[4]];
+      //   compDet[br[4]] = temp*1 + (br[2].substring(0, len-2))*1;
+      //   //document.getElementById("demo2").innerHTML = compDet[br[4]];
+      // }else{
+      //   compDet[br[4]]=(br[2].substring(0, len-2))*1;
+      // }
+      //corr end
+      let amountLen = br[2].length
+      let calorieLen = br[1].length
+      let amount = br[2].substring(0,amountLen-2)
+      let calorie = br[1].substring(0,calorieLen-4)
+
+      if(compDet[br[4]]){
+        //console.log("magulaaa",compDet[br[4]])
+        let oldAmount = compDet[br[4]][0];
+        let oldCalorie = compDet[br[4]][1];
+        let newAmount = oldAmount + amount*1;
+        let newCalorie = oldCalorie + calorie*1;
+        compDet[br[4]] = [newAmount,newCalorie];
+      }else{
+        compDet[br[4]] = [amount*1,calorie*1];
+      }
+    });
+    dinnerArr.forEach((br) => {
+      //shoppingListArr.push(br[2]);
+
+      //corr start
+      // let len = br[2].length
+      // if(compDet[br[4]]){
+      //   let temp = compDet[br[4]];
+      //   compDet[br[4]] = temp*1 + (br[2].substring(0, len-2))*1;
+      //   //document.getElementById("demo2").innerHTML = compDet[br[4]];
+      // }else{
+      //   compDet[br[4]]=(br[2].substring(0, len-2))*1;
+      // }
+      //corr end
+      let amountLen = br[2].length
+      let calorieLen = br[1].length
+      let amount = br[2].substring(0,amountLen-2)
+      let calorie = br[1].substring(0,calorieLen-4)
+
+      if(compDet[br[4]]){
+        //console.log("magulaaa",compDet[br[4]])
+        let oldAmount = compDet[br[4]][0];
+        let oldCalorie = compDet[br[4]][1];
+        let newAmount = oldAmount + amount*1;
+        let newCalorie = oldCalorie + calorie*1;
+        compDet[br[4]] = [newAmount,newCalorie];
+      }else{
+        compDet[br[4]] = [amount*1,calorie*1];
+      }
+    });
+
+  });
+
+  // if(compDet[br[4]]){
+  //   let temp = compDet[br[4][0]]; //gram
+  //   let temp2 = compDet[br[4][1]]; //cal
+  //   compDet[br[4]] = [temp*1 + (br[2].substring(0, len-2))*1,temp2*1 + (br[1].substring(0, len2-4))*1];
+  //   //document.getElementById("demo2").innerHTML = compDet[br[4]];
+  // }else{
+  //   compDet[br[4]]=[(br[2].substring(0, len-2))*1,(br[1].substring(0, len2-4))*1];
+  // }
+
+
+  //res.status(200).json(shoppingListArr);
+  res.status(200).json(compDet);
+}
 module.exports = { 
   saveDietPlans,
-  getDietPlans 
+  getDietPlans,
+  saveShoppingList,
 };
--- src/router/dietPlan.routes.js
+++ src/router/dietPlan.routes.js
@@ -7,19 +7,20 @@ const {
   getNonActivePlans,
   getActivePlans,
 } = require("../controller/dietPlan.controller");
-const {saveDietPlans, getDietPlans} = require("../controller/diet.controller")
+const {saveDietPlans, getDietPlans, saveShoppingList} = require("../controller/diet.controller")
 const auth = require("../middleware/auth");
 
 const router = Router();
 
-router.post("/quiz", getInputs);
-router.get("/getplans/getDietPlans",getDietPlans);
+router.post("/quiz", getInputs); 
+router.get("/getplans/getDietPlans",getDietPlans); 
 router.get("/:id", getDietPlanById);
 router.post("/generatedietplan", generateDietPlan);
 router.post("/savedietplan", saveDietPlans);
 router.get("/getUserDietPlans/:id",getDietPlanByUserId);
 router.get("/getuserplans/nonactive/:id",getNonActivePlans);
 router.get("/getuserplans/active/:id",getActivePlans);
+router.post("/generateShoppingList",saveShoppingList);
 
 
 module.exports.dietPlanRouter = router;
