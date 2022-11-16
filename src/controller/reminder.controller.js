eeb042b1ebcf7a6c3ecde9082ac99c6434c547b4
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-11-16 16:23:13 +0530

    update

--- src/controller/reminder.controller.js
+++ src/controller/reminder.controller.js
@@ -1,19 +1,90 @@
 const Reminder = require("../models/reminder");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const newReminder = async (req, res) => {
-    const user_id = req.body.user_Id;
-  
-    try {
-      const newReminder = await Reminder.create({user_Id: user_id });
-      res.status(200).json(newReminder);
-    } catch (e) {
-      console.log(e);
-      res.status(400).send({ e, success: false });
-    }
+  const user_id = req.body.user_Id;
+
+  try {
+    const newReminder = await Reminder.create({ user_Id: user_id });
+    res.status(200).json(newReminder);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({ e, success: false });
+  }
+};
+
+const updateBreakfast = async (req, res) => {
+  const _id = req.body._id;
+  const conditions = {
+    breakfastMinute: req.body.breakfastMinute,
+    breakfastHour: req.body.breakfastHour,
+    breakfastOn: req.body.breakfastOn,
   };
 
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).json({ error: "Invalid Reminder ID" });
+  }
+
+  const reminder = await Reminder.findByIdAndUpdate({ _id }, conditions, {
+    new: true,
+  });
+
+  if (!reminder) {
+    return res.status(400).json({ error: "No such reminder" });
+  }
+
+  res.status(200).json(reminder);
+};
+const updateLunch = async (req, res) => {
+    const _id = req.body._id;
+    const conditions = {
+      lunchMinute: req.body.lunchMinute,
+      lunchHour: req.body.lunchHour,
+      lunchOn: req.body.lunchOn,
+    };
+  
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(400).json({ error: "Invalid Reminder ID" });
+    }
+  
+    const reminder = await Reminder.findByIdAndUpdate({ _id }, conditions, {
+      new: true,
+    });
+  
+    if (!reminder) {
+      return res.status(400).json({ error: "No such reminder" });
+    }
+  
+    res.status(200).json(reminder);
+  };
+
+  const updateDinner = async (req, res) => {
+    const _id = req.body._id;
+    const conditions = {
+      dinnerMinute: req.body.dinnerMinute,
+      dinnerHour: req.body.dinnerHour,
+      dinnerOn: req.body.dinnerOn,
+    };
+  
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(400).json({ error: "Invalid Reminder ID" });
+    }
+  
+    const reminder = await Reminder.findByIdAndUpdate({ _id }, conditions, {
+      new: true,
+    });
+  
+    if (!reminder) {
+      return res.status(400).json({ error: "No such reminder" });
+    }
+  
+    res.status(200).json(reminder);
+  };
+  
 
 module.exports = {
-    newReminder,
+  newReminder,
+  updateBreakfast,
+  updateLunch,
+  updateDinner
 };
--- src/router/reminder.routes.js
+++ src/router/reminder.routes.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const {
   newReminder,
+  updateBreakfast,
+  updateLunch,
+  updateDinner
 
 } = require("../controller/reminder.controller");
 const auth = require("../middleware/auth");
@@ -8,7 +11,9 @@ const auth = require("../middleware/auth");
 const router = Router();
 
 router.post("/newReminder", newReminder);
-
+router.post("/updatebreakfast", updateBreakfast);
+router.post("/updatelunch", updateLunch);
+router.post("/updatedinner", updateDinner);
 
 
 module.exports.reminderRouter = router;
