6213ad6fce5df1c9877b6a0e77b9e76a61a1cee2
GimhanRanasinghe <gimhanravi@gmail.com>
2022-11-17 13:24:44 +0530

    change test

--- src/app.test.js
+++ src/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("EatSmart Server", () => {
-  it("signIn", () => {
+  it("signUp", () => {
     return request(app)
       .post("/user/signUp")
       .send({
