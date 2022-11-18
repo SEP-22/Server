39aea5350ffbb957ad4d490abc5c906d42536b25
GimhanRanasinghe <gimhanravi@gmail.com>
2022-11-18 23:08:05 +0530

    start script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("../src/app");
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV;
 
 app.listen(port, () => {
